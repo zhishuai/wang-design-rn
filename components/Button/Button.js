b3cfb52d2c214f172baf7455faac077f09d51e84
Your Name <you@example.com>
2022-01-16 21:15:20 +0800

    test2

--- components/Button/Button.js
+++ components/Button/Button.js
@@ -4,13 +4,13 @@
 
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
-import {View, TouchableOpacity} from 'react-native';
+import {View, TouchableOpacity, Text} from 'react-native';
 
 export default class Button extends TouchableOpacity {
     render() {
         return (
-            <View>
-                {super.render()}
+            <View style={{backgroundColor: 'red'}}>
+                <Text>testButton</Text>
             </View>
         )
     }    
